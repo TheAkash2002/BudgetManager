7a8823e9f75b9a43f961103869f190b47935102c
@TheAkash2002 <mahapatraakash@gmail.com>
2019-12-24 16:48:09 +0000

    Wrote insertExpenseData() and insertTargetData() for DatabaseHelper

--- app/src/main/java/com/princeakash/budgetmanager/ActivityAddExpense.java
+++ app/src/main/java/com/princeakash/budgetmanager/ActivityAddExpense.java
@@ -7,12 +7,18 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.Toast;
+
+import static android.widget.Toast.LENGTH_SHORT;
+import static java.lang.Integer.parseInt;
 
 public class ActivityAddExpense extends AppCompatActivity {
 
     Spinner spinnerCategory;
     Button btnAddExpense;
     EditText editAmount;
+    String selectedCategory;
+    DatabaseHelper myDb;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,7 +31,9 @@ public class ActivityAddExpense extends AppCompatActivity {
                 new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
-
+                        boolean isInserted = myDb.insertExpenseData(selectedCategory, parseInt(editAmount.getText().toString()));
+                        if(isInserted == true)
+                            Toast.makeText(ActivityAddExpense.this, "Data Inserted", LENGTH_SHORT).show();
                     }
                 }
         );
--- app/src/main/java/com/princeakash/budgetmanager/DatabaseHelper.java
+++ app/src/main/java/com/princeakash/budgetmanager/DatabaseHelper.java
@@ -1,10 +1,16 @@
 package com.princeakash.budgetmanager;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 import androidx.annotation.Nullable;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 public class DatabaseHelper extends SQLiteOpenHelper {
 
@@ -20,14 +26,16 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     public static final String colCategoryID = "CategoryID";
     public static final String colCategoryName = "Name";
 
+    Calendar calendar;
+
     public DatabaseHelper(@Nullable Context context) {
         super(context, dbName, null, 33);
     }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        db.execSQL("CREATE TABLE " + expenseTable + " ("+colExpenseID+" INTEGER PRIMARY KEY, "+colExpenseAmount+" INTEGER, "+colExpenseCategory+" TEXT, "+colExpenseDate+" DATE)");
-        db.execSQL("CREATE TABLE " + categoryTable + " ("+colCategoryID+" INTEGER PRIMARY KEY, "+colCategoryName+" TEXT)");
+        db.execSQL("CREATE TABLE " + expenseTable + " ("+colExpenseID+" INTEGER PRIMARY KEY AUTOINCREMENT, "+colExpenseAmount+" INTEGER, "+colExpenseCategory+" TEXT, "+colExpenseDate+" DATE)");
+        db.execSQL("CREATE TABLE " + categoryTable + " ("+colCategoryID+" INTEGER PRIMARY KEY AUTOINCREMENT, "+colCategoryName+" TEXT)");
     }
 
     @Override
@@ -36,4 +44,56 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         db.execSQL("DROP TABLE IF EXISTS "+categoryTable);
         onCreate(db);
     }
+
+    public boolean insertExpenseData(String category, int amount){
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        calendar = Calendar.getInstance();
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd-MM-yyyy");
+        String date = simpleDateFormat.format(calendar.getTime());
+
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(colExpenseAmount, amount);
+        contentValues.put(colExpenseCategory, category);
+        contentValues.put(colExpenseDate, date);
+        long insert = db.insert(expenseTable, colExpenseID, contentValues);
+        if(insert == -1)
+            return false;
+        else
+            return true;
+    }
+
+    public void deleteExpenseData(){
+
+    }
+
+    public void updateExpenseData(){
+
+    }
+
+    public void viewAllExpenseData(){
+
+    }
+
+    public void viewMonthlyExpenseData(String month, String year){
+
+    }
+
+    public boolean insertTargetData(int amount, String month, String year){
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        String date = year + "-" + month + "-01";
+
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(colExpenseAmount, amount);
+        contentValues.put(colExpenseCategory, "Target");
+        contentValues.put(colExpenseDate, date);
+        long insert = db.insert(expenseTable, colExpenseID, contentValues);
+        if(insert == -1)
+            return false;
+        else
+            return true;
+    }
+
+
 }
